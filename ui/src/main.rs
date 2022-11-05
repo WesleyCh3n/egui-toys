f00c0f4b16253e3058b578e7cdfd48e146d6fe0e
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-11-05 23:02:07 +0800

    feat: channel

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -1,12 +1,14 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+use std::sync::{Arc, Mutex};
+
 use eframe::egui;
 fn main() {
     let options = eframe::NativeOptions::default();
     eframe::run_native(
         "Play around ui",
         options,
-        Box::new(|_cc| Box::new(MyApp::default())),
+        Box::new(|_cc| Box::new(MyApp::new(_cc))),
     );
 }
 
@@ -20,26 +22,28 @@ struct MyApp {
     show_window: bool,
     slider_value: u32,
     radio_value: MyEnum,
-    progress_value: i32,
+    process_data: Arc<Mutex<i32>>,
+    sender_data: std::sync::mpsc::Sender<i32>,
 }
 
-impl Default for MyApp {
-    fn default() -> Self {
+impl MyApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        let (sx_data, rx) = std::sync::mpsc::channel();
+        let result = Arc::new(Mutex::new(0));
+        spawn_repaint_thread(rx, result.clone(), cc.egui_ctx.clone());
+
         Self {
             show_window: false,
             slider_value: 70,
             radio_value: MyEnum::Happy,
-            progress_value: 0,
+            process_data: result,
+            sender_data: sx_data,
         }
     }
 }
 
 impl eframe::App for MyApp {
-    fn update(
-        &mut self,
-        ctx: &eframe::egui::Context,
-        _frame: &mut eframe::Frame,
-    ) {
+    fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
         // TopBottomPanel must before CentralPanel
         egui::TopBottomPanel::top("my panel").show(ctx, |ui| {
             ui.menu_button("My menu", |ui| {
@@ -57,17 +61,9 @@ impl eframe::App for MyApp {
             ui.group(|ui| {
                 ui.label("In group");
                 ui.horizontal(|ui| {
-                    ui.radio_value(
-                        &mut self.radio_value,
-                        MyEnum::Happy,
-                        "Happy",
-                    );
+                    ui.radio_value(&mut self.radio_value, MyEnum::Happy, "Happy");
                     ui.radio_value(&mut self.radio_value, MyEnum::Sad, "Sad");
-                    ui.radio_value(
-                        &mut self.radio_value,
-                        MyEnum::Excited,
-                        "Excited",
-                    );
+                    ui.radio_value(&mut self.radio_value, MyEnum::Excited, "Excited");
                 });
             });
             ui.group(|ui| {
@@ -78,22 +74,22 @@ impl eframe::App for MyApp {
                 });
                 ui.horizontal(|ui| {
                     ui.label("Drager");
-                    ui.add(
-                        egui::DragValue::new(&mut self.slider_value)
-                            .clamp_range(0..=100),
-                    );
+                    ui.add(egui::DragValue::new(&mut self.slider_value).clamp_range(0..=100));
                 });
             });
             ui.weak("somthing\nnextline");
             ui.horizontal(|ui| {
-                if ui.button("+").clicked() {
-                    self.progress_value += 1;
+                ui.set_visible(false);
+                if ui.button("start").clicked() {
+                    long_process(self.sender_data.clone(), 10);
                 }
-                if ui.button("-").clicked() {
-                    self.progress_value -= 1;
+                if ui.button("reset").clicked() {
+                    *self.process_data.lock().unwrap() = 0;
                 }
             });
-            ui.add(egui::ProgressBar::new(self.progress_value as f32 / 100.));
+            ui.add(egui::ProgressBar::new(
+                *self.process_data.lock().unwrap() as f32 * (10. / 100.),
+            ));
         });
         egui::Window::new("new window")
             .open(&mut self.show_window)
@@ -101,4 +97,27 @@ impl eframe::App for MyApp {
                 ui.label("Hello world");
             });
     }
+}
+
+fn long_process(sender: std::sync::mpsc::Sender<i32>, length: i32) {
+    std::thread::spawn(move || {
+        for i in 1..=length {
+            sender.send(i).unwrap();
+            std::thread::sleep(std::time::Duration::from_secs(1));
+        }
+    });
+}
+
+fn spawn_repaint_thread<T: std::marker::Send + 'static>(
+    rx: std::sync::mpsc::Receiver<T>,
+    data: Arc<Mutex<T>>,
+    ctx: egui::Context,
+) {
+    std::thread::spawn(move || loop {
+        if let Ok(a) = rx.recv() {
+            let mut data_ = data.lock().unwrap();
+            *data_ = a;
+            ctx.request_repaint();
+        }
+    });
 }
